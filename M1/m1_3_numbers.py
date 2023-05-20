8c006e2b37e53610053f0cf507474471eeb11b62
Nuno Rocha <nuno.teles.rocha@gmail.com>
2023-05-20 03:40:34 +0100

    Adiciondos os ex 1.3 e 1.4

---
+++ M1/m1_3_numbers.py
@@ -0,0 +1,10 @@
+"""
+Atividade 1.3 - Numbers
+
+"""
+
+print(3 + 5)
+print(12 - 4)
+print(4 * 2)
+print(int(16 / 2))
+print('Nuno Rocha')
---
+++ M1/m1_4_comentarios.py
@@ -0,0 +1,20 @@
+"""
+Atividade 1.4 - Comentários
+
+"""
+# Exercício sobre números do Mod 1
+
+print(3 + 5)
+print(12 - 4)
+print(4 * 2)
+print(int(16 / 2))
+print('Nuno Rocha')
+
+# Exercício sobre variáveis do Mod 1
+
+nome = 'Nuno Rocha'
+print(nome)
+message = 'Mentores & Tutores'
+print('Olá eu sou o', nome, 'e estou a fazer o curso de Python na', message)
+message = 'A primeira atividade foi concluída com sucesso'
+print(message)
